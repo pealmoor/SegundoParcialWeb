4f0d8b278a0caf742087594ddb33a60678b0948f
pepedro123 <81392589+pepedro123@users.noreply.github.com>
2023-11-20 12:08:31 -0500

    Update UsuarioServlet.java
    No se habian guardado los servlet

--- repository/segundoparcialweb/src/main/java/co/banco/test/controller/UsuarioServlet.java
+++ repository/segundoparcialweb/src/main/java/co/banco/test/controller/UsuarioServlet.java
@@ -1,5 +1,162 @@
 package co.banco.test.controller;
 
-public class UsuarioServlet {
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import co.banco.test.dao.UsersDao;
+import co.banco.test.modelo.Users;
+
+
+@WebServlet("/")
+public class UsuarioServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	private UsersDao usersDao;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public UsuarioServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		this.usersDao = UsuarioDaoFactory.getUsuarioDao("mysql");
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		String action = request.getServletPath();
+		try {
+			switch(action) {
+			case "/new":
+				showNewForm(request, response);
+				break;
+			case "/insert":
+				insertarUsuario(request, response);
+				break;
+			case "/delete":
+				eliminarUsuario(request, response);
+				break;
+			case "/edit":
+				showEditForm(request, response);
+				break;
+			case "/update":
+				actualizarUsuario(request, response);
+				break;
+			
+			default:
+				listUsuarios(request, response);
+				break;
+			}
+		}catch(SQLException e) {
+			throw new ServletException(e);
+		}
+	}
+
+	
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+	
+	private void showNewForm(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher("users.jsp");
+		dispatcher.forward(request, response);
+	}
+	
+	private void insertarUsuario(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, SQLException, IOException {
+		
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		String email = request.getParameter("email");
+		
+		Users users = new Users(username, password, email);
+		usersDao.insert(users);
+		
+		response.sendRedirect("list");
+	}
+	
+	private void showEditForm(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException{
+		int id = Integer.parseInt(request.getParameter("id"));
+		
+		Users usuarioActual = usersDao.select(id);
+		request.setAttribute("users", usuarioActual);
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher("users.jsp");
+		dispatcher.forward(request, response);
+	}
+	
+	private void actualizarUsuario(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, SQLException, IOException {
+		
+		int id = Integer.parseInt(request.getParameter("id"));
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		String email = request.getParameter("email");
+		
+		Users users = new Users(id,username, password, email);
+		usersDao.update(users);
+		
+		response.sendRedirect("list");
+	}
+	
+	private void enviarEmailDatos(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException, SQLException {
+		
+		int id = Integer.parseInt(request.getParameter("id"));
+		
+		Users usuarioActual = usersDao.select(id);
+		
+		request.setAttribute("users", usuarioActual);
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher("bill.jsp");
+		
+		dispatcher.forward(request, response);
+			
+		}
+	
+	private void eliminarUsuario(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, SQLException, IOException {
+		
+		int id = Integer.parseInt(request.getParameter("id"));
+		
+		usersDao.delete(id);
+		response.sendRedirect("list");
+	}
+	
+	private void listUsuarios(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, SQLException, IOException{
+		
+		List<Users> listUsuarios = usersDao.selectAll();
+		request.setAttribute("listUsuarios", listUsuarios);
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher("usuariolist.jsp");
+		dispatcher.forward(request, response);
+		
+	}
 }
