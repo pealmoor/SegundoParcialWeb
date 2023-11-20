c2e19b563e10cd86bb839ea392149f05f3e419bf
pepedro123 <81392589+pepedro123@users.noreply.github.com>
2023-11-20 12:09:39 -0500

    Update Bill.java
    Getters setters y constructores manuales por que no me cogio lombok :/

--- repository/segundoparcialweb/src/main/java/co/banco/test/modelo/Bill.java
+++ repository/segundoparcialweb/src/main/java/co/banco/test/modelo/Bill.java
@@ -22,6 +22,43 @@ public class Bill implements Serializable {
 	private Integer value;
 	private String type;
 	private String observation;
+
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public Date getDate_bill() {
+		return date_bill;
+	}
+	public void setDate_bill(Date date_bill) {
+		this.date_bill = date_bill;
+	}
+	public Integer getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(Integer user_id) {
+		this.user_id = user_id;
+	}
+	public Integer getValue() {
+		return value;
+	}
+	public void setValue(Integer value) {
+		this.value = value;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getObservation() {
+		return observation;
+	}
+	public void setObservation(String observation) {
+		this.observation = observation;
+	}
 	
 	
 	
